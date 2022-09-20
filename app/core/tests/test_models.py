640398d4e36d552eac3b5e2c2eadb501d74564ab
hendrik <hendrikuwabara@gmail.com>
2022-09-20 15:56:26 -0500

    Added creating admin

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -23,7 +23,7 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
 
-        sample_emails =[
+        sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@EXAMPLE.com', 'Test2@example.com'],
             ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
@@ -34,7 +34,6 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
-
 
     def test_new_user_without_email_raises_error(self):
         """Test tha creating a user without an email raises a ValueError"""
